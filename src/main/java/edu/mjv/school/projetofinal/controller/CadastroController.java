c4cdd974b62a91364676421f94a45f8027429fa7
Athos Franco <athos.franco123@gmail.com>
2022-03-17 20:29:55 -0300

    primeiro commit

---
+++ src/main/java/edu/mjv/school/projetofinal/controller/CadastroController.java
@@ -0,0 +1,52 @@
+package edu.mjv.school.projetofinal.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.mjv.school.projetofinal.model.Cadastro;
+
+@RestController
+@RequestMapping("/cadastros")
+public class CadastroController {
+	@PostMapping()
+	public void gravar(@RequestBody Cadastro cadastro) {
+		System.out.println("Gravando registro");
+		System.out.println("cadastro: " + cadastro.getNome());
+ 
+	}
+	@PutMapping()
+	public void alterar(@RequestBody Integer id, Cadastro cadastro) {
+		System.out.println("Alterando registros");
+		System.out.println(cadastro);
+
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void excluir(@PathVariable("id") Integer id) {
+		System.out.println("Excluindo dados");
+		System.out.println("Id: " + id);
+
+	}
+	
+	@GetMapping("/filtro")
+	public List<Cadastro> filtrar(@RequestParam("nm") String nome) {
+		System.out.println("Listando cadastros pelo nome " + nome);
+		return null;
+	}
+
+	@GetMapping()
+	public List<Cadastro> listar() {
+		System.out.println("Listando dados");
+		return null;
+		
+	}
+}
---
+++ src/main/java/edu/mjv/school/projetofinal/model/Cadastro.java
@@ -0,0 +1,20 @@
+package edu.mjv.school.projetofinal.model;
+
+public class Cadastro {
+	private Integer id;
+	private String nome;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
