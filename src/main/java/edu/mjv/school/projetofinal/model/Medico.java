9b10dda216c248e17e7a6772498e5b94e82f404d
Athos Franco <athos.franco123@gmail.com>
2022-03-20 08:06:39 -0300

    criaçao das tabelas Medico e Paciente

--- src/main/java/edu/mjv/school/projetofinal/model/Medico.java
+++ src/main/java/edu/mjv/school/projetofinal/model/Medico.java
@@ -1,5 +1,79 @@
 package edu.mjv.school.projetofinal.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_medico")
 public class Medico {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_medico")
+	private Integer idMedico;
+	
+	@Column(name = "codigo_crm", nullable = false, length = 10)
+	private String codigoCRM;	
+	
+	@Column(name = "id_pessoa", nullable = false)
+	@JoinColumn(name = "tb_pessoa", referencedColumnName = "id_pessoa")
+	private Pessoa pessoa;
+	
+	@Column(name = "especialidade", nullable = false)
+	@JoinColumn(name = "tb_especialidade", referencedColumnName = "titulo")
+	private Especialidade especialidade;
+	
+	@Column(nullable = false)
+	boolean ativo;
+
+	//getters e setters
+	public Integer getIdMedico() {
+		return idMedico;
+	}
+
+	public void setIdMedico(Integer idMedico) {
+		this.idMedico = idMedico;
+	}
+
+	public String getCodigoCRM() {
+		return codigoCRM;
+	}
+
+	public void setCodigoCRM(String codigoCRM) {
+		this.codigoCRM = codigoCRM;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/edu/mjv/school/projetofinal/model/Paciente.java
+++ src/main/java/edu/mjv/school/projetofinal/model/Paciente.java
@@ -1,5 +1,64 @@
 package edu.mjv.school.projetofinal.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_paciente")
 public class Paciente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_paciente")
+	private Integer idPaciente;
+
+	@Column(name = "matricula_PS", nullable = false, length = 10)
+	private String matriculaPlanoDeSaude;
+
+	@Column(name = "id_pessoa", nullable = false)
+	@JoinColumn(name = "tb_pessoa", referencedColumnName = "id_pessoa")
+	private Pessoa pessoa;
+
+	@Column(nullable = false)
+	boolean ativo;
+
+	// getters and setters
+
+	public Integer getIdPaciente() {
+		return idPaciente;
+	}
+
+	public void setIdPaciente(Integer idPaciente) {
+		this.idPaciente = idPaciente;
+	}
+
+	public String getMatriculaPlanoDeSaude() {
+		return matriculaPlanoDeSaude;
+	}
+
+	public void setMatriculaPlanoDeSaude(String matriculaPlanoDeSaude) {
+		this.matriculaPlanoDeSaude = matriculaPlanoDeSaude;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
 
 }
