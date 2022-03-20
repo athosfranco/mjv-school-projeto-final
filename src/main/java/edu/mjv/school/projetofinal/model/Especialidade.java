99753045cd73eaa1611824e215a2e6c07b38da95
Athos Franco <athos.franco123@gmail.com>
2022-03-20 08:15:16 -0300

    criaçao entidade Especialidade

--- src/main/java/edu/mjv/school/projetofinal/model/Especialidade.java
+++ src/main/java/edu/mjv/school/projetofinal/model/Especialidade.java
@@ -1,5 +1,52 @@
 package edu.mjv.school.projetofinal.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_especialidade")
 public class Especialidade {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_especialidade")
+	private Integer idEspecialidade;
+	
+	@Column(nullable = false, length = 20)
+	private String titulo;
+	
+	@Column(nullable = false)
+	boolean ativo;
+
+	//getters & setters
+	public Integer getIdEspecialidade() {
+		return idEspecialidade;
+	}
+
+	public void setIdEspecialidade(Integer idEspecialidade) {
+		this.idEspecialidade = idEspecialidade;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+
+	
 }
